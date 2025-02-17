92071fbda14f22f5e76dd034bd49ca61e0191328
Riccardo Beccaria <87329576+RiccardoBecca@users.noreply.github.com>
2025-02-17 15:35:04 +0100

    Create find_encounter_files.py

---
+++ MarkovStateModel/find_encounter_files.py
@@ -0,0 +1,19 @@
+import sys
+import os
+
+total_solutes = int(sys.argv[1]) # p1 + p2 + crowders
+subname = sys.argv[2]
+#files = sys.argv[3:]
+to_transfer = []
+
+for file in os.listdir("./"):
+    if subname in file:
+        with open(file,"r") as fin:
+            lines = fin.readlines()
+        if len(lines) > total_solutes + 2: # +2 because of the header in encounter complex file
+            to_transfer.append(file.replace(subname,''))
+#print(to_transfer)
+bash_string=''
+for num in to_transfer:
+    bash_string=bash_string+" "+num
+print(bash_string)
